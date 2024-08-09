601c6c723b5641acc6b04cee4f2f8dfcbbc9c973
LelouKieh <qx@zju.edu.cn>
2024-08-09 10:53:52 -0700

    Update colors for different types of Villgers for visualization

--- Visualize.py
+++ Visualize.py
@@ -1,11 +1,12 @@
 import pygame
-from Lumberjack import Lumberjack
 from Angler import Angler
 from Arborist import Arborist
 from Builder import Builder
 from Explorer import Explorer
 from Farmer import Farmer
 from FishingShip import FishingShip
+from Lumberjack import Lumberjack
+from configuration.world_configuration import LINE_COLOR
 
 
 class Visualizer:
@@ -13,13 +14,31 @@ class Visualizer:
     def __init__(self, world):
         self.world = world
 
-    def draw_path(self, screen, start_pos, end_pos):
-        pygame.draw.line(screen, (255, 0, 0), start_pos, end_pos, 2)  # Draw a red line
+    def draw_path(self, screen, start_pos, end_pos, color):
+        pygame.draw.line(screen, color, start_pos, end_pos, 2)  # Draw a red line
 
     def render(self, screen):
         for entity in self.world.entities.values():
-            if isinstance(entity, Lumberjack):
-                start_pos = (int(entity.world_location.x), int(entity.world_location.y))
-                end_pos = (int(entity.destination.x + entity.world.world_position.x),
+            match entity:
+                case Lumberjack():
+                    color = LINE_COLOR[0]
+                case Angler():
+                    color = LINE_COLOR[1]
+                case Arborist():
+                    color = LINE_COLOR[2]
+                case Builder():
+                    color = LINE_COLOR[3]
+                case Explorer():
+                    color = LINE_COLOR[4]
+                case Farmer():
+                    color = LINE_COLOR[5]
+                case FishingShip():
+                    color = LINE_COLOR[6]
+                case _:
+                    color = (0, 0, 0) # default color: Black
+            start_pos = (int(entity.world_location.x), int(entity.world_location.y))
+            end_pos = (int(entity.destination.x + entity.world.world_position.x),
                         int(entity.destination.y + entity.world.world_position.y))
-                self.draw_path(screen, start_pos, end_pos)
+            self.draw_path(screen, start_pos, end_pos, color)
+        
+        
--- configuration/world_configuration.py
+++ configuration/world_configuration.py
@@ -6,3 +6,12 @@ DAY_DURATION = DAYTIME_DURATION + NIGHTTIME_DURATION
 # Configuration of maximum managed tiles for each farmer
 TILES_PER_FARMER = 20
 
+# Configuration for colors
+LINE_COLOR = [(255, 0, 0), # Red - Lumberjack
+              (255, 255, 0), # Yellow - Angler
+              (255, 255, 255), # White - Arborist
+              (127, 255, 255), # Light Blue - Builder
+              (185, 28, 202), # Purple - Explorer
+              (255, 167, 0), # Orange - Farmer
+              (100, 45, 0) # Brown - FishingShip
+              ]
